413c1e1cdb751babbb479454e97c24eb403185c6
fanyafeng <1181631922@qq.com>
2015-03-10 11:56:45 +0800

    测试类

---
+++ test/cn/edu/sjzc/teacher/uiFragment/FindStudentTest.java
@@ -0,0 +1,14 @@
+package cn.edu.sjzc.teacher.uiFragment;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FindStudentTest {
+
+	@Test
+	public void testInit() {
+		fail("Not yet implemented");
+	}
+
+}
